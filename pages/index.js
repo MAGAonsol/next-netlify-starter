7e1659db470c1e808c1f08da1ff2b0c8c7d2dadc
MAGAonsol <tvinny2532@yahoo.com>
2024-11-05 20:45:10 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Untitledcss.css">
+    <title>$MAGA MAGAassonsol</title>
+   
+</head>
+<body>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div class="full-page" style="background-image: url('Screenshot_5-11-2024_173144_images.search.yahoo.com.jpeg');">
+        <div class="address-box">
+            Coin Contract Address: <span id="contract-address">0xYourCoinAddressHere</span>
+        </div>
+        <div class="content">
+            <a href="https://x.com/MAGAassonsol" target="_blank">X Account</a>
+            <a href="#" target="_blank">Pump.fun Account</a>
+            <a href="#" target="_blank">Telegram</a>
+        </div>
     </div>
-  )
-}
+
+    <!-- Section 2 (Photo Background) -->
+    <div class="full-page" style="background-image: url('225603-patriotic-ass-nude (1).jpg');"></div>
+
+    <!-- Section 3 (Photo Background) -->
+    <div class="full-page" style="background-image: url('QmQK4HdPerZiZ8JAfMhsaDmftMGmTUcmUwjurB8wQNzLtF.jpg');"></div>
+
+</body>
+</html>
